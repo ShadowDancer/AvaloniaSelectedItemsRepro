daa5feab390b4a792c87da8521dbad6e7d581aba
Przemysław Onak <przemekonak@gmial.com>
2020-07-01 01:07:00 +0200

    Add project files.

---
+++ AvaloniaNoSelectedEventsRepro/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvaloniaNoSelectedEventsRepro.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private int counter;
+
+        public int Counter { get => counter; set => this.RaiseAndSetIfChanged(ref counter, value); }
+    }
+}
---
+++ AvaloniaNoSelectedEventsRepro/Views/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using AvaloniaNoSelectedEventsRepro.ViewModels;
+
+namespace AvaloniaNoSelectedEventsRepro.Views
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            (DataContext as MainWindowViewModel).Counter++;
+        }
+    }
+}
